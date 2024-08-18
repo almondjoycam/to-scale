423aa99c6e77d06c32bcfc2c36c8d33e96403c33
Camber Boles <camberb51@gmail.com>
2024-08-17 22:24:46 -0400

    Possible embiggening code

--- 2D to Scale/Assets/Scripts/PlayerControls.cs
+++ 2D to Scale/Assets/Scripts/PlayerControls.cs
@@ -5,19 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    // Movement configuration
     Vector2 move;
     [SerializeField]
     float speed;
+    [SerializeField]
+    float jumpForce;
+    public bool allDirectionMovement;
+
+    // Physics
+    Rigidbody2D rb;
+    bool isGrounded;
+
+    // Gameplay
     public float scaleFactor {
         get;
         private set;
     }
+    ObjectResizer embiggen = new ObjectResizer();
+    ObjectResizer shrinkify = new ObjectResizer();
     GameMenu menu;
 
     // Start is called before the first frame update
     void Start()
     {
         menu = FindObjectOfType<GameMenu>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        embiggen.SetScaleFactor(2);
+        shrinkify.SetScaleFactor(0.5f);
     }
 
     // Update is called once per frame
@@ -29,6 +45,20 @@ public class PlayerControls : MonoBehaviour
     public void OnMove(InputValue value)
     {
         move = value.Get<Vector2>();
+        if (!allDirectionMovement)
+        {
+            move.y = 0;
+        }
+    }
+
+    public void OnJump()
+    {
+        if (isGrounded)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
 
     public void OnScale(InputValue value)
@@ -44,9 +74,21 @@ public class PlayerControls : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Resizeable"))
+        if (other.collider.gameObject.CompareTag("Resizeable"))
         {
-            // Resize here
+            if (scaleFactor < 0)
+            {
+                shrinkify.ResizeObject(other.collider.gameObject);
+            }
+            else if (scaleFactor > 0)
+            {
+                embiggen.ResizeObject(other.collider.gameObject);
+            }
+        }
+        else if (other.collider.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            rb.isKinematic = true;
         }
     }
 }
